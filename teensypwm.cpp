2b2162b8eefb072a127eb1f4bf8b367cfc94c4c6
ijulanday <ulanday.ian@gmail.com>
2022-07-25 13:17:11 -0700

    hybrid stuff

--- teensypwm.cpp
+++ teensypwm.cpp
@@ -1,47 +1,44 @@
 #include "teensypwm.h"
 
 PWMdata pwmData; // pwm data instance
-int delayTime = 0;
+int delayTime = 0;  //this is like a trim kinda thing, probably keep at zero
 
-// throttle% = -0.00202(pwm) + 3.11717 for fly sky controller
-void calcThrottlePercent() {
-  pwmData.throttlePercent = -0.00202*pwmData.manualThrottlePWM + 3.11717;
+bool acceptable(unsigned long pwm) 
+{
+  return (pwm >= 900) & (pwm <= 2000);
 }
 
-void calcSteeringAngle() {
-  pwmData.steeringAngle = map(pwmData.manualSteeringPWM, 1000, 2000, -20.0, 20.0);
-}
-
-void risingCH1();
 void fallingCH1()
 {
-  pwmData.manualSteeringPWM = (micros() - delayTime) - pwmData.ch1Timer;
-  calcSteeringAngle();
-  attachInterrupt(digitalPinToInterrupt(STEERING_INTERRUPT_PIN), risingCH1, RISING);
+  pwmData.prevCh1PWM = pwmData.ch1PWM;
+  unsigned long temp = (micros() - delayTime) - pwmData.ch1Timer;
+  pwmData.ch1PWM = acceptable(temp) ? temp : pwmData.prevCh1PWM;
+  attachInterrupt(digitalPinToInterrupt(CH1_INTERRUPT_PIN), risingCH1, RISING);
 }
 void risingCH1()
 {
   pwmData.ch1Timer = micros();
-  attachInterrupt(digitalPinToInterrupt(STEERING_INTERRUPT_PIN), fallingCH1, FALLING);
+  attachInterrupt(digitalPinToInterrupt(CH1_INTERRUPT_PIN), fallingCH1, FALLING);
 }
-void risingCH2();
+
 void fallingCH2()
 {
-  pwmData.manualThrottlePWM = (micros() - delayTime) - pwmData.ch2Timer;
-  pwmData.mappedThrottlePWM = map(pwmData.manualThrottlePWM, 1500, 2000, 1000, 2000); // actual input mapped to desired output
-  calcThrottlePercent();
-  attachInterrupt(digitalPinToInterrupt(THROTTLE_INTERRUPT_PIN), risingCH2, RISING);
+  pwmData.prevCh2PWM = pwmData.ch2PWM;
+  unsigned long temp = (micros() - delayTime) - pwmData.ch2Timer;
+  pwmData.ch2PWM = acceptable(temp) ? temp : pwmData.prevCh2PWM;
+  attachInterrupt(digitalPinToInterrupt(CH2_INTERRUPT_PIN), risingCH2, RISING);
 }
 void risingCH2()
 {
   pwmData.ch2Timer = micros();
-  attachInterrupt(digitalPinToInterrupt(THROTTLE_INTERRUPT_PIN), fallingCH2, FALLING);
+  attachInterrupt(digitalPinToInterrupt(CH2_INTERRUPT_PIN), fallingCH2, FALLING);
 }
-void risingCH3();
+
 void fallingCH3()
 {
-  pwmData.CH3PWM = (micros() - delayTime) - pwmData.ch3Timer;
-  pwmData.ch3toggle = pwmData.CH3PWM > 1500 ? true : false;
+  pwmData.prevCh3PWM = pwmData.ch3PWM;
+  unsigned long temp = (micros() - delayTime) - pwmData.ch3Timer;
+  pwmData.ch3PWM = acceptable(temp) ? temp : pwmData.prevCh3PWM;
   attachInterrupt(digitalPinToInterrupt(CH3_INTERRUPT_PIN), risingCH3, RISING);
 }
 void risingCH3()
@@ -49,4 +46,6 @@ void risingCH3()
   pwmData.ch3Timer = micros();
   attachInterrupt(digitalPinToInterrupt(CH3_INTERRUPT_PIN), fallingCH3, FALLING);
 }
+
+
 
--- teensypwm.h
+++ teensypwm.h
@@ -10,52 +10,36 @@
 #define TMAP_MAX_OUT      1000
 #define TMAP_MIN_OUT      2000
 
-void calcThrottlePercent();
-void calcSteeringAngle();
 void risingCH1();
 void risingCH2();
 void risingCH3();
+void fallingCH1();
+void fallingCH2();
+void fallingCH3();
 
 typedef enum
 {  
-  STEERING_SERVO_PIN = 3,
-  THROTTLE_SERVO_PIN = 6,
-  STEERING_INTERRUPT_PIN = 15,
-  THROTTLE_INTERRUPT_PIN = 16,
-  CH3_INTERRUPT_PIN = 17,
-  LED_GPS_FIX_PIN = 18,
-  LED_AUTOPILOT_PIN = 19,
-  LED_5V_VOLTAGE_PIN = 22,
-  LED_12V_VOLTAGE_PIN = 23,
-  SENSE_5V_VOLTAGE_PIN = A6,
-  SENSE_12V_VOLTAGE_PIN = A7,
-  BATTERY_VOLTAGE_PIN = A0,
-  EXTERNAL_LED_PIN = 4,
-  C1_SWITCH_PIN = 2,
-  C2_SWITCH_PIN = 5
+  CH1_INTERRUPT_PIN = 24,
+  CH2_INTERRUPT_PIN = 25,
+  CH3_INTERRUPT_PIN = 33,
+  SWITCH_POS_1 = A0,
+  SWITCH_POS_2 = A1,
+  ENGINE_S_POS = A2
 } Pins;
 
 typedef struct
 {
-//THROTTLE PID VARIABLES
-double mappedThrottlePWM = 900;
-double manualThrottlePWM = 1500;
-double autopilotThrottlePWM = 1500;
-//STEERING PID VARIABLES
-double steeringPWM;
-double steeringFilteredPWM;
-double manualSteeringPWM = 1500;
-double autopilotSteeringPWM = 1500;
-// other variables
-bool ch3toggle = 0;
-double CH3PWM;
-double throttlePercent;
-double steeringAngle;
+unsigned long ch1PWM = 1500;
+unsigned long ch2PWM = 1500;
+unsigned long ch3PWM = 1500;
+unsigned long prevCh1PWM = 1500;
+unsigned long prevCh2PWM = 1500;
+unsigned long prevCh3PWM = 1500;
+
 unsigned long futabaCH3PWM = 1000;
 unsigned long ch1Timer;
 unsigned long ch2Timer;
 unsigned long ch3Timer;
-
 } PWMdata;
 
 extern PWMdata pwmData; //Create a pointer to the system data
